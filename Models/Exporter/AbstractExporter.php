82722e871b09c6df2137fe322321c0596a44dae5
Dana Negrescu <dana@boxalino.com>
2020-02-19 19:26:40 +0100

    wip order exporter

--- Models/Exporter/AbstractExporter.php
+++ Models/Exporter/AbstractExporter.php
@@ -95,4 +95,5 @@ class AbstractExporter
     {
         $this->dirPath = $dirPath;
     }
+
 }
--- Models/Exporter/Order.php
+++ Models/Exporter/Order.php
@@ -1,7 +1,6 @@
 <?php
 namespace Boxalino\Models\Exporter;
 
-
 class Order implements ExporterInterface
 {
 
